6fc0ae0196fa8ef73fdc5535243d3b26fb07f31b
jalen57 <82652095+jalen57@users.noreply.github.com>
2024-01-11 21:47:21 -0500

    Add files via upload

--- application.py
+++ application.py
@@ -1,106 +1,47 @@
 from flask import Flask
-from flask import render_template, jsonify
+from flask import render_template, jsonify, make_response
 import urllib.request, json
 import urllib.error
 import pandas as pd
-
-app = Flask(__name__)
-
+import pymysql
 
 
-
-@app.route('/schedule')
-def schedule():
-   with urllib.request.urlopen(
-       "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
-   ) as url:
-       data = json.load(url)
-       scoreboard = data['scoreboard']
-       games = scoreboard['games']
-       games_dict = []
-       for game in games:
-           temp_dict = {
-               'away': game['awayTeam']['teamTricode'],
-               'a_score': game['awayTeam']['score'],
-               'home': game['homeTeam']['teamTricode'],
-               'h_score':game['homeTeam']['score'],
-               'time': game['period'],
-               'a_to': game['awayTeam']['timeoutsRemaining'],
-               'h_to': game['homeTeam']['timeoutsRemaining'],
-               'game_id': game['gameId'],
-               'a_bonus': game['awayTeam']['inBonus'],
-               'h_bonus': game['homeTeam']['inBonus'],
-               'clock': game['gameStatusText']
-           }
-           games_dict.append(temp_dict)
-       return(games_dict)
+application = Flask(__name__)
 
 
-@app.route('/data_json')
+  
+
+@application.route('/data_json')
 def data_json():
-   sched = schedule()
-   table_dict = []
-   index = 0
-   for s in sched:
-       try:
-           full_url = "https://cdn.nba.com/static/json/liveData/playbyplay/playbyplay_" + s['game_id'] + ".json"
-           with urllib.request.urlopen(full_url) as url:
-               data = json.load(url)
-           new = pd.DataFrame.from_dict(data['game']['actions'])
-           pbp = data['game']['actions'].pop()
-           temp_q = pbp['period']
-           away = s['away']
-           home = s['home']
-           a_fouls = (new.period[(new.period==temp_q) & (new.actionType=="foul") & (new.teamTricode==away)].count())
-           h_fouls = (new.period[(new.period==temp_q) & (new.actionType=="foul") & (new.teamTricode==home)].count())
-           if a_fouls >= 4:
-               a_b = "BONUS"
-           else:
-               a_b = "No"
-           if h_fouls >= 4:
-               h_b = "BONUS"
-           else:
-               h_b = "No"
-           index += 1
-           temp_dict = {
-               'id': index,
-               'away': s['away'],
-               'a_score': pbp['scoreAway'],
-               'home': s['home'],
-               'h_score': pbp['scoreHome'],
-               'time': pbp['period'],
-               'a_to': s['a_to'],
-               'h_to': s['h_to'],
-               'a_bonus': h_b,
-               'h_bonus': a_b,
-               'clock': pbp['clock'].replace("PT","").replace("M",":").replace("S",""),
-               'state': pbp['actionType'],
-               'desc': pbp['subType'],
-               'detail': pbp['description'],
-               'qual': pbp['qualifiers']
-           }
-           table_dict.append(temp_dict)
-       except urllib.error.HTTPError as e:
-           index += 1
-           temp_dict = {
-               'id': index,
-               'away': s['away'],
-               'a_score': s['a_score'],
-               'home': s['home'],
-               'h_score': s['h_score'],
-               'time': 1,
-               'a_to': s['a_to'],
-               'h_to': s['h_to'],
-               'a_bonus': 'none',
-               'h_bonus': 'none',
-               'clock': s['clock'],
-               'state': 'not inplay',
-               'desc': 'not inplay',
-               'detail': 'not inplay',
-               'qual': 'not inplay'
-           }
-           table_dict.append(temp_dict)
-   return(jsonify(table_dict))
+    db = pymysql.connect(host='database-6.crga42aec7fl.us-east-1.rds.amazonaws.com', user="admin",password="Jsoccer5",port=3306, database="database6")
+    query = "SELECT * FROM data_bulk"
+    try: 
+        df = pd.read_sql(query, db)
+        db.close()
+        data_bulk = df.to_dict('records')
+        table_dict = []
+        for s in data_bulk:
+            temp_dict = {
+                'id': s['id'],
+                'away': s['away'],
+                'a_score': s['a_score'],
+                'home': s['home'],
+                'h_score': s['h_score'],
+                'time': s['quarter'],
+                'a_to': s['a_to'],
+                'h_to': s['h_to'],
+                'a_bonus': s['h_bonus'],
+                'h_bonus': s['a_bonus'],
+                'clock': s['g_clock'],
+                'state': s['action'],
+                'desc': s['sub'],
+                'detail': s['desc'],
+                'qual': s['quals'] 
+            }
+            table_dict.append(temp_dict)
+        return(jsonify(table_dict))
+    except:
+         return(jsonify)
 
 
 
@@ -109,16 +50,17 @@ def data_json():
 
 
 
-@app.route('/')
+@application.route('/')
 def index():
    dummy_data = data_json()
-   return render_template(
-       'index.html',
-       matches=dummy_data.json,
-   )
+   response = make_response(render_template('index.html', matches=dummy_data.json))
+   response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate, public, max-age=0"
+   response.headers["Pragma"] = "no-cache"
+   response.headers["Expires"] = "0"
+   return(response)
 
 
-if __name__ == '__main__':
-    app.run()
 
 
+if __name__ == "__main__":
+        application.run()
